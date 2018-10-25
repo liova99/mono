9dc592a99b2c1d1b583533b89a906b5ee0b269ed
Levan.Kelesidis <levan.kelesidis@media-concept.com>
2018-10-25 12:57:16 +0200

    Auto Bewegung erstellt

--- Car_race_levan/Program.cs
+++ Car_race_levan/Program.cs
@@ -14,7 +14,7 @@ namespace Car_race_levan
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new CarRaceLevan())
                 game.Run();
         }
     }
