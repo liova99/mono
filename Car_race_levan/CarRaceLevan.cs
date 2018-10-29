6e4f45b004f67bd8c9a127e85ffe3d7094b6f5f5
Levan <levan.list@gmail.com>
2018-10-29 20:29:33 +0100

    create track class and add it as background

--- Car_race_levan/CarRaceLevan.cs
+++ Car_race_levan/CarRaceLevan.cs
@@ -14,6 +14,8 @@ namespace Car_race_levan
         SpriteBatch spriteBatch;
         GraphicsDeviceManager graphics;
 
+        Track track = new Track();
+
        // KeyboardInteraction keyboardInteraction = new KeyboardInteraction();
 
         Car cabrio = new Car();
@@ -43,7 +45,17 @@ namespace Car_race_levan
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+
+            // TODO: Add your initialization logic 
+
+            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            //graphics.IsFullScreen = true;
+
+            // graphics.ToggleFullScreen(); 
+            graphics.ApplyChanges();
+
+
             cabrioPosition = cabrio.CarPosition;
             blueCarPosition = blueCar.CarPosition;
 
@@ -68,8 +80,9 @@ namespace Car_race_levan
 
             // Load the car <texture2D oder 3D> ("name_of_the_file")
 
-            cabrio.CarTexture = Content.Load<Texture2D>("auto_cabrio");
-            blueCar.CarTexture = Content.Load<Texture2D>("auto_blue");
+            cabrio.CarTexture = Content.Load<Texture2D>("cabrio");
+            blueCar.CarTexture = Content.Load<Texture2D>("blue");
+            track.trackTexture = Content.Load<Texture2D>("track_big");
 
         }
 
@@ -171,15 +184,21 @@ namespace Car_race_levan
             //Draw the car
             spriteBatch.Begin();
 
+            //==Track Big ==
+            //Vector2 trackOrigin = new Vector2(track.trackTexture.Width / 2, track.trackTexture.Height / 2);
+            Rectangle mainFrame = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+            spriteBatch.Draw(track.trackTexture, mainFrame, Color.White);
+
+
             // == Cabrio==
             Vector2 cabrioOrigin = new Vector2(cabrio.CarTexture.Width / 2, cabrio.CarTexture.Height / 2);
 
-            spriteBatch.Draw(cabrio.CarTexture, cabrioPosition, null, Color.White, cabrio.CarAngle, cabrioOrigin, 0.2f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(cabrio.CarTexture, cabrioPosition, null, Color.White, cabrio.CarAngle, cabrioOrigin, 0.6f, SpriteEffects.None, 0f);
 
             // == Blue Car==
             Vector2 blueCarOrigin = new Vector2(blueCar.CarTexture.Width / 2, blueCar.CarTexture.Height / 2);
 
-            spriteBatch.Draw(blueCar.CarTexture, blueCarPosition, null, Color.White, blueCar.CarAngle, blueCarOrigin, 0.2f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(blueCar.CarTexture, blueCarPosition, null, Color.White, blueCar.CarAngle, blueCarOrigin, 0.6f, SpriteEffects.None, 0f);
 
 
 
---
+++ Car_race_levan/Track.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_race_levan
+{
+    
+
+    class Track
+    {
+        public Texture2D trackTexture;
+
+        public Track()
+        {
+        }
+
+
+
+    }
+
+    
+}
